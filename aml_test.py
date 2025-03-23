4c1ab49ab0763793bb13c948b570d122d3284a5b
KavyaRSaxena <kavyars@iitk.ac.in>
2025-03-23 16:26:22 +0530

    v1

---
+++ aml_test.py
@@ -0,0 +1,369 @@
+import os
+import time
+import random
+import librosa
+import numpy as np
+import tensorflow as tf
+from keras import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, BatchNormalization, Dropout, Reshape, TimeDistributed
+from keras.regularizers import l2
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import mir_eval
+from glob import glob
+
+filepath_pre = '../weights/meta_model/pre/meta_weights-{epoch:02d}'
+filepath_conf = '../weights/meta_model/conf/meta_weights-{epoch:02d}'
+
+tf.keras.backend.set_floatx('float32')
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+gpus = tf.config.list_physical_devices('GPU')
+
+gpu_number = 0
+
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        tf.config.set_visible_devices(gpus[gpu_number], 'GPU')
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+###################################################
+
+def get_cenfreq(StartFreq, StopFreq, NumPerOct):
+    Nest = int(np.ceil(np.log2(StopFreq / StartFreq)) * NumPerOct)
+    central_freq = []
+    for i in range(0, Nest):
+        CenFreq = StartFreq * pow(2, float(i) / NumPerOct)
+        if CenFreq < StopFreq:
+            central_freq.append(CenFreq)
+        else:
+            break
+    return central_freq
+
+def onehotlabel(pitch_range):
+    values = np.asarray(pitch_range)
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    return onehot_encoded
+
+Nfft = 1024
+win_len = 512
+hop_len = 80
+win_size = 500
+bpo = 96
+pitch_range = get_cenfreq(51.91, 1975.53, bpo)
+pitch_range = np.concatenate([np.zeros(1), pitch_range])
+onehot_pitch_range = onehotlabel(pitch_range)  # one hot vector
+#################################################
+class melody_extraction(Model):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = Conv2D(filters=64, name='conv1', kernel_size=(5, 5), input_shape=(win_size, 513), padding='same',
+                            kernel_initializer='he_normal', kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5))
+        self.bn1 = BatchNormalization(name='bn1')
+        self.conv2 = Conv2D(filters=128, name='conv2', kernel_size=(5, 5), padding='same',
+                            kernel_initializer='he_normal', kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5))
+        self.bn2 = BatchNormalization(name='bn2')
+        self.conv3 = Conv2D(filters=192, name='conv3', kernel_size=(5, 5), padding='same',
+                            kernel_initializer='he_normal', kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5))
+        self.bn3 = BatchNormalization(name='bn3')
+        self.conv4 = Conv2D(filters=256, name='conv4', kernel_size=(5, 5), padding='same',
+                            kernel_initializer='he_normal', kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5))
+        self.bn4 = BatchNormalization(name='bn4')
+        self.linear1 = Dense(512, name='dense1', activation='relu', kernel_initializer='he_normal',
+                             kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5))
+        self.final = TimeDistributed(
+            Dense(len(pitch_range), name='dense2', activation='softmax', kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-5), bias_regularizer=l2(1e-5)))
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = tf.keras.activations.relu(x)
+        x = MaxPooling2D((1, 4))(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = tf.keras.activations.relu(x)
+        x = MaxPooling2D((1, 4))(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = tf.keras.activations.relu(x)
+        x = MaxPooling2D((1, 4))(x)
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = tf.keras.activations.relu(x)
+        x = MaxPooling2D((1, 4))(x)
+        x = Reshape((-1, x.shape[2] * x.shape[3]))(x)
+        int_output = self.linear1(x)
+        x = self.final(int_output)
+        return x, int_output
+
+    def build_graph(self, raw_shape):
+        x = Input(shape=raw_shape)
+        return Model(inputs=[x], outputs=self.call(x))
+
+
+meta_trained_classifier = melody_extraction()
+meta_trained_classifier.build_graph([win_size, 513, 1])  # .summary()
+
+for i in range(len(meta_trained_classifier.layers) - 1):
+    meta_trained_classifier.layers[i].trainable = False
+fe_model = melody_extraction()
+fe_model.build_graph([win_size, 513, 1])
+fe_model.load_weights('./models/adaptive_weights/weights/meta_model/pre/meta_weights-500')  # load the pre-train weights
+
+for i in range(len(meta_trained_classifier.layers) - 1):
+    fe_model.layers[i].trainable = False
+
+class ConfidenceModel(Model):
+    def __init__(self):
+        super().__init__()
+        self.pretrain = fe_model
+        self.dense2 = Dense(256, activation='relu', kernel_initializer='he_normal', kernel_regularizer=l2(1e-5),
+                            bias_regularizer=l2(1e-5))
+        self.final = Dense(1, activation='sigmoid', kernel_initializer='he_normal', kernel_regularizer=l2(1e-5),
+                           bias_regularizer=l2(1e-5))
+
+    def call(self, x):
+        _, x = self.pretrain(x)
+
+        x = self.dense2(x)
+        x = Dropout(0.2)(x)
+        x = self.final(x)
+        return x
+
+    def build_graph(self, raw_shape):
+        x = Input(shape=raw_shape)
+        return Model(inputs=[x], outputs=self.call(x))
+
+
+meta_trained_conf = ConfidenceModel()
+meta_trained_conf.build_graph([win_size, 513, 1])  # .summary()
+meta_trained_conf.layers[0].trainable = False
+conf_model = ConfidenceModel()
+conf_model.build_graph([win_size, 513, 1])  # .summary()
+conf_model.layers[0].trainable = False
+conf_model.load_weights('./models/adaptive_weights/weights/meta_model/conf/meta_weights-500')
+
+mean = -18.800999505542258
+std = 12.473455860620371
+
+alpha = 1.e-4  # 5.e-5  --> only 3 times retraining
+beta = 1.e-6  # 5.e-5
+loss_fn = tf.keras.losses.CategoricalCrossentropy()
+mse = tf.keras.losses.MeanSquaredError()
+inner_optimizer = tf.keras.optimizers.Adam(learning_rate=alpha)
+conf_inner_optimizer = tf.keras.optimizers.Adam(learning_rate=beta)
+inner_step = 10
+
+def conf_loss(yt, yp, yc, ts):
+    yc_star = tf.zeros((yt.shape[0], yt.shape[1]), dtype=tf.float32)
+    ts = tf.cast(ts, dtype=tf.int32)
+    w_mask = tf.zeros(yt.shape[1], dtype=tf.float32)
+    w_mask = tf.tensor_scatter_nd_update(w_mask, tf.expand_dims(ts, 1), tf.ones_like(ts, dtype=tf.float32))
+    w_mask = tf.expand_dims(w_mask, 0)
+    true_indx = tf.argmax(yt, axis=-1)
+    true_indx = tf.reduce_max(true_indx, axis=0)
+    row_no = tf.range(0, yt.shape[1], 1)
+    row_no = tf.cast(row_no, tf.int32)
+    true_indx = tf.cast(true_indx, tf.int32)
+    indexing = tf.concat([row_no[:, tf.newaxis], true_indx[:, tf.newaxis]], axis=1) 
+    y_p = tf.ones(tf.shape(yp[0]))
+    c_star = tf.gather_nd(y_p, indexing)
+    c_star = c_star[tf.newaxis, :, tf.newaxis]
+    loss = mse(c_star, yc, sample_weight=tf.constant(w_mask))
+    return loss
+
+
+def support_pretrain_step(x, y, ts):
+    x = tf.convert_to_tensor(x)
+    y = tf.convert_to_tensor(y)
+    x = x[tf.newaxis, :, :, :]
+    y = y[tf.newaxis, :, :]
+    with tf.device('/gpu:0'):
+        for _ in range(inner_step):
+            with tf.GradientTape(persistent=True) as tape:
+                ys_hat, _ = meta_trained_classifier.call(x)
+                loss = custom_loss(y, ys_hat, ts)
+            grads = tape.gradient(loss, meta_trained_classifier.trainable_variables)
+            inner_optimizer.apply_gradients(zip(grads, meta_trained_classifier.trainable_variables))
+    return loss
+
+def support_conftrain_step(x, y, ts):
+    x = tf.convert_to_tensor(x)
+    y = tf.convert_to_tensor(y)
+    x = x[tf.newaxis, :, :, :]
+    y = y[tf.newaxis, :, :]
+    with tf.device('/gpu:0'):
+        for _ in range(inner_step):
+            with tf.GradientTape(persistent=True) as tape:
+                ys_hat, _ = meta_trained_classifier.call(x)
+                yc_hat = meta_trained_conf.call(x)
+                loss = conf_loss(y, ys_hat, yc_hat, ts)
+            grads = tape.gradient(loss, meta_trained_conf.trainable_variables)
+            conf_inner_optimizer.apply_gradients(zip(grads, meta_trained_conf.trainable_variables))
+    return loss
+
+def verify_conf(x, y, indx):
+    yc = meta_trained_conf.call(tf.convert_to_tensor(x))
+    yc = tf.reshape(yc, -1)
+    yp, _ = meta_trained_classifier.call(x)
+    indx = indx[0]
+    for i in range(tf.shape(y)[0]):
+        for j in indx:
+            print(f'True indx..{tf.argmax(y[i][j], axis=-1)} Pred indx..{tf.argmax(yp[i][j], axis=-1)} Conf..{yc[j]}')
+
+def get_groundtruth_freq(Y):
+    true_indx = tf.argmax(Y[0], axis=-1)
+    gfv = pitch_range[true_indx]
+    return gfv
+
+rpa_tot = np.array([])
+
+def calc_spec(x, fs):
+    X = np.abs(librosa.stft(x, n_fft=Nfft, hop_length=hop_len, win_length=win_len, window='hann'))
+    X = librosa.power_to_db(X, ref=np.max)
+    X = X[:, :win_size]
+    return np.array(X)
+
+for i in range(len(audio_files)):
+    pitchfile = os.path.basename(os.path.splitext(audio_files[i])[0])
+
+
+def plot_melody(fe_model,S):
+    efv = []
+    t = []
+
+    S = S.T
+    S = tf.convert_to_tensor(S)
+    S = (S - mean) / std
+    S = S[tf.newaxis, :, :, tf.newaxis]
+
+    y_p, _ = fe_model.call(S)  # this is the one-hot vector of the prediction
+    # converting one hot vector to the continuous frequency values in Hz
+    for j in range(y_p.shape[0]):
+        for i in range(y_p.shape[1]):
+            indx = np.argmax(y_p[j, i, :])
+            efv.append(pitch_range[indx])
+            t.append(i * 0.01)
+
+    return y_p, efv, t
+
+def M(f):
+    f_ref = 100
+    return bpo * np.log2((f + 1.e-9) / f_ref)
+
+def Thres(a):
+    if -0.5 <= a and a <= 0.5:
+        t = 1
+    else:
+        t = 0
+    return t
+
+def RawPitch(F_predicted, F_ground):
+    if F_predicted == float(0) and F_ground == float(0):
+        return 1
+    else:
+        return Thres(M(F_predicted) - M(F_ground))
+
+def closest(arr, K):
+    idx = np.array([RawPitch(val, K) for val in arr])
+    idx = idx.argmax()
+    return idx
+
+def get_onehot(f):
+    yhot = np.zeros((len(f), len(pitch_range)))
+    for i in range(len(f)):
+        idx = closest(pitch_range, f[i])
+        yhot[i, :] = onehot_pitch_range[idx, :]
+    return yhot
+
+def calc_rpa(gfv,efv):
+    t = [0.01*i for i in range(len(efv))]
+    t = np.array(t)
+    gfv = np.array(gfv)
+    efv = np.array(efv)
+    (ref_v, ref_c,est_v, est_c) = mir_eval.melody.to_cent_voicing(t,gfv,t,efv)
+
+    RPA = mir_eval.melody.raw_pitch_accuracy(ref_v, ref_c,est_v, est_c)
+    RCA = mir_eval.melody.raw_chroma_accuracy(ref_v, ref_c,est_v, est_c)
+    OA = mir_eval.melody.overall_accuracy(ref_v, ref_c,est_v, est_c)
+
+def custom_loss(yt, yp, ts):
+    w_mask = tf.convert_to_tensor(ts)
+    w_mask = tf.cast(w_mask, dtype=tf.float32)
+    w_mask = tf.expand_dims(w_mask, 0)
+    classes = tf.reduce_sum(tf.cast(yt, tf.float32), axis=(0, 1))
+    cratio = 1.0 / (classes / tf.reduce_max(classes))
+    weights = tf.where(tf.math.is_finite(cratio), cratio, tf.constant(0.0))
+    w = tf.gather(weights, tf.argmax(yt, axis=-1))
+    w = tf.multiply(w, w_mask)
+    loss = loss_fn(yt, yp, sample_weight=w_mask)
+    return loss
+
+
+def aml_test(S, active_frames, gfv,fileid):
+    alpha = 1.e-4  # 5.e-5  --> only 3 times retraining
+    inner_optimizer = tf.keras.optimizers.Adam(learning_rate=alpha)
+    S = S.T
+    S = tf.convert_to_tensor(S)
+    S = (S-mean)/std
+    S = S[tf.newaxis, :, :, tf.newaxis]    
+    efv = []
+    # for every batch, start from the active-meta-trained models
+    if (fileid==0):
+        meta_pre_weights = fe_model.get_weights()
+        meta_conf_weights = conf_model.get_weights()
+    else:
+        fe_model.load_weights('models/updated_weights/weights')  # load the pre-train weights
+        meta_pre_weights = fe_model.get_weights()
+        meta_conf_weights = conf_model.get_weights()
+        alpha = alpha* 0.9 * fileid
+        inner_optimizer = tf.keras.optimizers.Adam(learning_rate=alpha)
+        print(f'alpha used: {alpha}')  
+    meta_trained_conf.set_weights(meta_conf_weights)
+    meta_trained_classifier.set_weights(meta_pre_weights)    
+
+    Y_true = get_onehot(gfv)  # get the onehot vector of the annotated frequencies - this will act as ground truth
+    Y_true = Y_true[tf.newaxis, :, :]       
+    num_batches = int(np.ceil(S.shape[1] / win_size))
+
+    with tf.device('/gpu:0'):
+        for epoch in range(inner_step):
+            tot_loss = 0
+            print(f'Epoch...{epoch}')
+            for i in range(num_batches):
+                start_indx = i * win_size
+                end_indx = min((i+1) * win_size, S.shape[1])
+                X = S[:,start_indx:end_indx,:,:]
+                
+                frames = active_frames[start_indx:end_indx]
+                
+                y_true = Y_true[:,start_indx:end_indx,:]
+                
+                if sum(frames)>0:
+                    with tf.GradientTape() as tape:
+                        ys_hat, _ = meta_trained_classifier.call(X)
+                        loss = custom_loss(y_true, ys_hat, frames)
+                        tot_loss+=loss
+            grads = tape.gradient(tot_loss, meta_trained_classifier.trainable_variables)
+            inner_optimizer.apply_gradients(zip(grads, meta_trained_classifier.trainable_variables))
+        ## melody estimation after adaptation
+        yq_hat, _ = meta_trained_classifier.call(S)
+        meta_trained_classifier.save_weights('models/updated_weights/weights')
+        # converting one hot vector to the continuous frequency values in Hz
+        for j in range(yq_hat.shape[0]):
+            for i in range(yq_hat.shape[1]):
+                indx = np.argmax(yq_hat[j, i, :])
+                efv.append(pitch_range[indx])         
+    return efv
+
---
+++ app.py
@@ -0,0 +1,411 @@
+import json
+import re
+import shutil
+from scipy import signal
+import numpy as np
+import math
+from flask import Flask, render_template, request, make_response, send_from_directory, jsonify, send_file
+import os
+import librosa
+import numpy as np
+import soundfile as sf
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+import mir_eval
+import csv
+from tcp_interface import *
+from aml_test import *
+import warnings
+
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
+
+wavfileDir = 'UploadedWavFile'
+chunkwavfileDir = './static/ChunkedWavFile'
+resynthwavfileDir = './static/ResynthWavFile'
+groundtruthDir = 'PredictedFile'
+supportindxDir = 'supportindxfile'
+filename = None
+
+fileIndx = 0
+
+@app.route('/')   #@app.route is the python decorator
+def index():
+  global fileIndx
+  fileIndx = 0
+  for filename in os.listdir(wavfileDir):
+            file_path = os.path.join(wavfileDir, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+  for filename in os.listdir(chunkwavfileDir):
+          file_path = os.path.join(chunkwavfileDir, filename)
+          if os.path.isfile(file_path):
+              os.remove(file_path)
+  for filename in os.listdir(resynthwavfileDir):
+          file_path = os.path.join(resynthwavfileDir, filename)
+          if os.path.isfile(file_path):
+              os.remove(file_path)
+  return render_template('index.html')
+  
+ 
+@app.route('/#browse')
+def browse():
+  return render_template('index.html')
+
+def get_spectrogram_json(y,sr):
+    N = 1024
+    win_len = 512  # 256
+    hop_len = 80
+    Sxx = np.abs(librosa.stft(y, n_fft=N, hop_length=hop_len, win_length=win_len, window='hann'))
+    Sxx = librosa.power_to_db(Sxx,ref=np.max)
+    f = np.arange(0, 1 + N / 2) * sr / N
+    t = [i * hop_len / sr for i in range(Sxx.shape[1])]
+    data = {
+        'x': t,
+        'y': f.tolist(),
+        'z': Sxx[:, :].tolist()
+    }
+    return Sxx,data
+  
+def calc_rpa(efv,filename,indexes):
+  filename = filename.split('.')[0]
+  gfv = []
+  
+  with open('./static/groundtruth/'+filename+'.csv','r') as fin:
+    reader = csv.reader(fin)
+    for row in reader:
+        row = [float(i) for i in row]
+        gfv.append(row[1])
+  
+  t = [0.01*i for i in range(len(efv))]
+  t = np.array(t)
+  gfv = np.array(gfv)
+  efv = np.array(efv)
+  
+  if len(gfv)<len(efv):
+    gfv = np.append(gfv,np.zeros(len(efv)-len(gfv)))
+  else:
+    efv = np.append(efv,np.zeros(len(gfv)-len(efv)))
+  
+  efv_trunc = [efv[i] for i in indexes]
+  efv_trunc = np.array(efv_trunc)
+  
+  gfv_trunc = [gfv[i] for i in indexes]
+  gfv_trunc = np.array(gfv_trunc)
+  
+  t_trunc = [0.01*i for i in range(len(efv_trunc))]
+  t_trunc = np.array(t_trunc) 
+  
+  (ref_v, ref_c,est_v, est_c) = mir_eval.melody.to_cent_voicing(t_trunc,gfv_trunc,t_trunc,efv_trunc)
+  RPA = mir_eval.melody.raw_pitch_accuracy(ref_v, ref_c,est_v, est_c)
+  RCA = mir_eval.melody.raw_chroma_accuracy(ref_v, ref_c,est_v, est_c)
+  OA = mir_eval.melody.overall_accuracy(ref_v, ref_c,est_v, est_c)
+  print(RPA,OA)
+  
+  (ref_v, ref_c,est_v, est_c) = mir_eval.melody.to_cent_voicing(t,gfv,t,efv)
+  RPA = mir_eval.melody.raw_pitch_accuracy(ref_v, ref_c,est_v, est_c)
+  RCA = mir_eval.melody.raw_chroma_accuracy(ref_v, ref_c,est_v, est_c)
+  OA = mir_eval.melody.overall_accuracy(ref_v, ref_c,est_v, est_c)
+  print(RPA,OA)
+  return gfv
+  
+def get_melody_json(Sxx,filename):
+ 
+  _ , efv, t = plot_melody(model,Sxx)
+  print('efv len',len(efv))
+  
+  t = [round(x, 2) for x in t]
+  data = {
+        't': t,
+        'f': efv
+        }
+  return data 
+  
+
+@app.route('/calculate_spectrogram', methods=['POST'])
+def calculate_spectrogram():
+  global fileIndx
+  print(f'File Index...{fileIndx}')
+  file = request.files['file']
+  fileName = file.filename
+  
+  file.save(os.path.join(wavfileDir, fileName))
+  
+  data, sr = librosa.load(os.path.join(wavfileDir, fileName), sr=8000)
+  
+  Sxx,spec_data = get_spectrogram_json(data,sr)
+  print('Melody spec',Sxx.shape)
+  # print('Orignal RPA,RCA')
+  melody_data = get_melody_json(Sxx,fileName)
+  
+  return make_response([spec_data,melody_data])
+
+
+@app.route('/get_conf_values',methods=['POST'])
+def get_conf_values():
+  file = request.files['file']
+  fileName = file.filename
+  
+  data,sr = librosa.load(os.path.join(wavfileDir,fileName),sr=8000)
+  
+  Sxx,_ = get_spectrogram_json(data,sr)
+  confValues = conf_values(model_conf,Sxx)
+  confValues = confValues.numpy()
+  confValues = confValues.reshape(-1)
+  return confValues.tolist()
+
+# @app.route('/get_sliced_audio_original',methods=['POST'])
+# def get_sliced_audio_original():
+#   file = request.files['file']
+#   filename = file.filename
+  
+#   y,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000)
+#   write(os.path.join(chunkwavfileDir,filename),sr,y)
+#   # Set the correct Content-Length header
+#   content_length = os.path.getsize(os.path.join(chunkwavfileDir, filename))
+  
+#   response = send_from_directory(chunkwavfileDir, filename)
+#   response.headers["Content-Length"] = content_length  
+#   return response
+
+
+# @app.route('/get_sliced_audio_resynth',methods=['POST'])
+# def get_sliced_audio_resynth():
+#   file = request.files['file']
+#   filename = file.filename
+  
+#   efv = request.form.get('array')
+#   efv = json.loads(efv)
+  
+#   for key in efv:
+#     efv = efv[key]
+  
+#   y,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000)
+#   t = np.array([0.01*i for i in range(len(efv))])
+#   t_new = np.array([i/sr for i in range(len(y))])  
+  
+#   v = np.array([1 if i!=0 else 0 for i in efv])
+#   f_new,vc = mir_eval.melody.resample_melody_series(t,efv,v,t_new,kind='nearest')
+#   y_resynth = pitch2wav(f_new,t_new)
+ 
+#   write(os.path.join(resynthwavfileDir,filename),sr,y_resynth)
+  
+#   content_length = os.path.getsize(os.path.join(resynthwavfileDir, filename))
+    
+#   response = send_from_directory(resynthwavfileDir, filename)
+#   response.headers["Content-Length"] = content_length
+#   return response
+   
+  
+  
+  
+@app.route('/get_sliced_audio_original',methods=['POST'])
+def get_sliced_audio_original():
+  file = request.files['file']
+  filename = file.filename
+  
+  start_time = float(request.form.get('start_time'))
+  end_time = float(request.form.get('end_time'))
+  
+  y,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000, offset= start_time, duration=end_time-start_time)
+  write(os.path.join(chunkwavfileDir,filename),sr,y)
+  # Set the correct Content-Length header
+  content_length = os.path.getsize(os.path.join(chunkwavfileDir, filename))
+  
+  response = send_from_directory(chunkwavfileDir, filename)
+  response.headers["Content-Length"] = content_length
+  
+  return response
+
+@app.route('/get_sliced_audio_resynth',methods=['POST'])
+def get_sliced_audio_resynth():
+  file = request.files['file']
+  filename = file.filename
+  
+  efv = request.form.get('array')
+  efv = json.loads(efv)
+  
+  for key in efv:
+    efv = efv[key]
+  
+  start_time = float(request.form.get('start_time'))
+  end_time = float(request.form.get('end_time'))
+  
+  y,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000)
+  t = np.array([0.01*i for i in range(len(efv))])
+  t_new = np.array([i/sr for i in range(len(y))])  
+  
+  v = np.array([1 if i!=0 else 0 for i in efv])
+  f_new,vc = mir_eval.melody.resample_melody_series(t,efv,v,t_new,kind='nearest')
+  y_resynth = pitch2wav(f_new,t_new)
+ 
+  write(os.path.join(resynthwavfileDir,filename),sr,y_resynth)
+  
+  y_resynth_chunked,sr = librosa.load(os.path.join(resynthwavfileDir,filename),sr=None, mono=True, offset= start_time, duration=end_time-start_time)
+
+  write(os.path.join(resynthwavfileDir,filename),sr,y_resynth_chunked)
+  
+  content_length = os.path.getsize(os.path.join(resynthwavfileDir, filename))
+    
+  response = send_from_directory(resynthwavfileDir, filename)
+  response.headers["Content-Length"] = content_length
+  return response
+  
+
+def pitch2wav(f,t, FLAG_extend=False):
+    if FLAG_extend:
+        for n in range(1,len(f)):
+            if f[n] == 0:
+                f[n] = f[n-1]
+
+    theta = [2*np.pi*f[0]*t[0]]
+    for i in range(1,len(f)):
+        delta_theta = 0.5* (2*np.pi*f[i]+2*np.pi*f[i-1]) * (t[i]-t[i-1])
+        theta.append(theta[i-1]+delta_theta)
+    return 0.5*np.sin(theta)
+  
+  
+# @app.route('/retrain_model',methods=['POST'])
+# def retrain_model():
+#   file = request.files['file']
+#   filename = file.filename
+  
+#   efv = request.form.get('orig_array')
+#   efv = json.loads(efv)  
+#   for key in efv:
+#     efv = efv[key]
+    
+#   # conf_values = request.form.get('conf_values')
+#   # conf_values = json.loads(conf_values)
+ 
+    
+#   retrainValues = request.form.get('retrain_values')
+#   retrainValues = json.loads(retrainValues)
+  
+#   unique_retrainValues = set(map(tuple, retrainValues))
+#   unique_retrainValues = [list(item) for item in unique_retrainValues]
+  
+#   unique_retrainValues = sorted(unique_retrainValues, key=lambda x: x[0]) 
+#   support_indx, gfv = zip(*unique_retrainValues)
+#   support_indx = list(support_indx)
+#   print('support index',support_indx)
+#   gfv = list(gfv)
+  
+#   # init_index = int(request.form.get('initial_index'))  
+#   # end_index = int(request.form.get('end_index'))
+  
+#   data,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000)
+  
+#   Sxx,_ = get_spectrogram_json(data,sr)
+    
+#   final_gfv = [0] * len(efv)
+  
+#   for index,value in zip(support_indx,gfv):
+#     final_gfv[index] = value       
+  
+#   updated_gfv, updated_conf = aml_test(Sxx,support_indx,final_gfv,init_index,end_index) 
+#   # updated_conf = updated_conf[init_index:end_index]
+#   # updated_conf = updated_conf.numpy()
+#   return make_response([updated_gfv, updated_conf.tolist(), support_indx])
+
+
+@app.route('/retrain_model',methods=['POST'])
+def retrain_model():
+  file = request.files['file']
+  filename = file.filename
+  
+  global fileIndx
+  
+  original_gfv = request.form.get('orig_array')
+  original_gfv = json.loads(original_gfv)  
+  for key in original_gfv:
+    original_gfv = original_gfv[key]
+    
+  conf_val = request.form.get('conf_values')
+  conf_val = json.loads(conf_val)  
+
+  retrainValues = request.form.get('retrain_values')
+  retrainValues = json.loads(retrainValues)  
+  unique_retrainValues = set(map(tuple, retrainValues))
+  unique_retrainValues = [list(item) for item in unique_retrainValues]  
+  unique_retrainValues = sorted(unique_retrainValues, key=lambda x: x[0])   
+  support_indx, gfv = zip(*unique_retrainValues)
+  support_indx = list(support_indx)
+  support_indx = [int(i) for i in support_indx]
+  print('Total support index',len(support_indx))
+  
+  # rows = []
+  # for i in support_indx:
+  #   rows.append([i,conf_val[i]])  
+  
+  # with open(os.path.join(supportindxDir,filename.split('.')[0]+'.csv'),'w') as file:
+  #   writer = csv.writer(file)
+  #   writer.writerows(rows)
+  
+  gfv = list(gfv)  
+  original_indexes = set(support_indx)
+  query_indx = [x for x in range(len(original_gfv)) if x not in original_indexes]
+  # print(f'Query Index..{query_indx}')
+   
+  
+  data,sr = librosa.load(os.path.join(wavfileDir,filename),sr=8000)
+  Sxx,_ = get_spectrogram_json(data,sr)
+  
+  # get the rpa,rca,oa on the query set on the initial prediction
+  # _ , initial_pred, _ = plot_melody(model,Sxx)  
+  # print(f'rpa,rca,oa on the query set on the initial prediction')
+  # actual_gfv = calc_rpa(initial_pred,filename,query_indx)  
+  # print(f'Accuracy on the query set on before adaptation')
+  # actual_gfv = calc_rpa(original_gfv,filename,query_indx)
+  
+  # for i in range(len(initial_pred)):
+  #   print(i*0.01,actual_gfv[i], initial_pred[i], original_gfv[i])
+  
+  final_gfv = [0] * len(original_gfv)
+  active_frames = [0] * len(original_gfv)
+  # print(f'length of finalgfv and active frames...{len(final_gfv)} {len(gfv)} {len(active_frames)}')
+  
+  for index,value in zip(support_indx,gfv):
+    final_gfv[index] = value     
+    active_frames[index] = 1
+    
+  updated_gfv = aml_test(Sxx,active_frames,final_gfv,fileIndx)
+  print(f'Accuracy on the query set after retraining for {fileIndx+1} times')
+  # calc_rpa(updated_gfv,filename,query_indx)
+
+  fileIndx+=1
+  return make_response([updated_gfv])
+  # return '',204
+
+
+@app.route('/download',methods=['POST'])
+def download():
+  file = request.files['file']
+  filename = file.filename  
+  filename = filename.split('.')[0]
+  print('filename',filename)
+  
+  efv = request.form.get('freq')
+  efv = json.loads(efv)
+  for key in efv:
+    efv = efv[key]
+  
+  rows = []
+  
+  for i in range(1,len(efv)):
+    rows.append([i*0.01,efv[i]])
+    
+  filename = filename +'.csv'
+    
+  with open(os.path.join(groundtruthDir,filename),'w') as file:
+    writer = csv.writer(file)
+    writer.writerows(rows)
+  
+  return '',204
+
+
+if __name__ == "__main__":
+  app.run(debug=True,port=2000)
